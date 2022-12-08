aec12b8b54ea2a1ee6d608b513a08589ef9b4a83
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-12-07 21:02:17 -0300

    Update onboarding_page.dart

--- lib/ui/onboarding/onboarding_page.dart
+++ lib/ui/onboarding/onboarding_page.dart
@@ -52,56 +52,56 @@ class OnboardingPage extends StatelessWidget {
                 const SizedBox(height: 22,),
                 Center(
                   child: Container(
-                          decoration: BoxDecoration(
-                           gradient: LinearGradient(
-                            begin: Alignment.topCenter,
-                              end: Alignment.bottomCenter,
-                              colors: [
-                                  ColorsApp.instance.greenlinear,
-                                  ColorsApp.instance.greenlinear2,
-                              ],
-                            ),
-                            borderRadius: BorderRadius.circular(30),
-                            boxShadow: const <BoxShadow>[
-                              BoxShadow(
-                                color: Color.fromRGBO(0, 0, 0, 0.57),
-                                blurRadius: 5
-                              )
-                              
-                            ]
-                        ),
-                        child: Button(
-                          height: context.height * .066,
-                          widht: context.width * .9,
-                          onPressed: () {
-                            //widget.presenter.checkLogin();
-                            Navigator.of(context).pushReplacement(MaterialPageRoute(
-                              builder: (BuildContext context) => const HomePage()));
-                          },
-                          style: ElevatedButton.styleFrom(
-                             backgroundColor: Colors.transparent,
-                             disabledForegroundColor: Colors.transparent,
-                             shadowColor: Colors.transparent,
-                             //make color or elevated button transparent
-                          ),
-                          labelStyle: context.textStyles.textStart,
-                          label: 'Get Started',
-                        ),
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                            ColorsApp.instance.greenlinear,
+                            ColorsApp.instance.greenlinear2,
+                        ],
                       ),
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                          color: ColorsApp.instance.greenlinear.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 3,
+                          offset: const Offset(2, 3),
+                        )
+                      ]
                     ),
-                    const SizedBox(height: 17,),
-                    Text(
-                      'Already Have Account? Log In',
-                      style: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.normal,
-                        color: ColorsApp.instance.green2,
+                    child: Button(
+                      height: context.height * .066,
+                      widht: context.width * .9,
+                      onPressed: () {
+                        //widget.presenter.checkLogin();
+                        Navigator.of(context).pushReplacement(MaterialPageRoute(
+                          builder: (BuildContext context) => const HomePage()));
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          disabledForegroundColor: Colors.transparent,
+                          shadowColor: Colors.transparent
+                      ),
+                      labelStyle: context.textStyles.textStart,
+                      label: 'Get Started',
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 17,),
+                Text(
+                  'Already Have Account? Log In',
+                   style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal,
+                       color: ColorsApp.instance.green2,
                       ),  
                     ),
-              ],
-            ),
-          )
-        ),
-      );
+            ],
+          ),
+        )
+      ),
+    );
   }
 }
