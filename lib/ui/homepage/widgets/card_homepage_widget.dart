bd43d688b7ae14b7951be234423904c2a67cb247
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-12-09 12:46:19 -0300

    Atualização do Card incluindo a seta

--- lib/ui/homepage/widgets/card_homepage_widget.dart
+++ lib/ui/homepage/widgets/card_homepage_widget.dart
@@ -11,7 +11,7 @@ class CardHomepageWidget extends StatelessWidget {
    Widget build(BuildContext context) {
        return Container(
             padding: EdgeInsets.only(
-                top: 150, left: 20, right: 20, bottom: context.height * .5),
+                top: 150, left: 20, right: 20, bottom: context.height * .48),
             child: Container(
               decoration: BoxDecoration(
                 color: ColorsApp.instance.greencard,
@@ -27,19 +27,23 @@ class CardHomepageWidget extends StatelessWidget {
               ),
               child: Column(
                 children: [
-                  Row(
-                    children: [
-                      const SizedBox(
-                         height: 65,
-                         width: 25,
-                      ),
-                      Text('Total Balance', style: context.textStyles.titleCardWhite),
-                      Image.asset('assets/images/chevrondown.png'),
-                      const SizedBox(
-                         width: 145,
-                      ),
-                      Image.asset('assets/images/dots3.png'),
-                    ],
+                  const SizedBox(height: 30,),
+                  Container(
+                    height: 25,
+                    color: Colors.transparent,
+                    child: Row(
+                      children: [
+                        const SizedBox(
+                           width: 25,
+                        ),
+                        Text('Total Balance', style: context.textStyles.titleCardWhite),
+                        Image.asset('assets/images/chevrondown.png'),
+                        const SizedBox(
+                           width: 145,
+                        ),
+                        Image.asset('assets/images/dots3.png'),
+                      ],
+                    ),
                   ),
                   Row(
                     children: [
@@ -53,13 +57,13 @@ class CardHomepageWidget extends StatelessWidget {
                     children: [
                       const SizedBox(
                          width: 25,
-                         height: 90,
+                         height: 95,
                       ),
                       Container(
-                        padding: const EdgeInsets.all(16),
+                        padding: const EdgeInsets.all(11),
                         decoration: BoxDecoration(
                           color: ColorsApp.instance.greenfundo,
-                          borderRadius: BorderRadius.circular(15),
+                          borderRadius: BorderRadius.circular(20),
                           boxShadow: <BoxShadow>[
                             BoxShadow(
                               color: ColorsApp.instance.greenfundo.withOpacity(0.3),
@@ -69,6 +73,7 @@ class CardHomepageWidget extends StatelessWidget {
                             )
                           ]
                         ),
+                        child: Image.asset("assets/images/arrowdown.png"),
                       )
                     ],
                   )
