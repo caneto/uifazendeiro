3f7e7a677bd2060995b8e96c081fea53f882795a
Carlos Alberto da Costa Pinto Neto <caneto@gmail.com>
2022-12-26 16:58:52 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,31 @@ import 'package:uifinanceiro/ui/ui_financeiro.dart';
 import 'ui/controller/theme_controller.dart';
 
 Future<void> main() async {
- 
+  
+  
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    bool inDebug = false;
+    assert((() {
+      inDebug = true;
+      return true;
+    }()));
+    if(inDebug) {
+      return ErrorWidget(details.exception);
+    }
+    return Container(
+      alignment: Alignment.center,  
+      child: Text(
+        'Error\n${details.exception}',
+        style: const TextStyle(
+          color: Colors.orangeAccent,
+          fontWeight: FontWeight.bold,
+          fontSize: 20
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  };
+
   WidgetsFlutterBinding.ensureInitialized();
   //verificar se o app esta configurada para o modo dark antes de iniciar o app
   await ThemeController.loadDarkMode();
